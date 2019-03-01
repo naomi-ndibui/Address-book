7d18fd02bfa7cd49d33bfdbfd08f20c8f34e2ef1
Naomi Ndibui <naomi.ndibui@gmail.com>
2019-03-01 16:44:11 +0300

    addede commits

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,5 +1,40 @@
+// business logic
 function Contact(first, last) {
-    this.firstName = first;
-    this.lastName = last;
+  this.firstName = first;
+  this.lastName = last;
+  this.addresses = [];
+}
+function Address(street, city, county) {
+    this.street = street;
+    this.city = city;
+    this.county = county;
   }
-  
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+var jane = new Contact("Jane", "Njeri");
+var home = new Address("671 Ngong Road", "Nairobi", "Nairobi");
+jane.addresses.push(home);
+
+// user interface logic
+$(document).ready(function() {
+  $("form#new-contact").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedFirstName = $("input#new-first-name").val();
+    var inputtedLastName = $("input#new-last-name").val();
+
+    var newContact = new Contact(inputtedFirstName, inputtedLastName);
+
+    $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
+    $(".contact").last().click(function() {
+      $("#show-contact").show();
+      $("#show-contact h2").text(newContact.firstName);
+      $(".first-name").text(newContact.firstName);
+      $(".last-name").text(newContact.lastName);
+    });
+
+    $("input#new-first-name").val("");
+    $("input#new-last-name").val("");
+  });
+});
